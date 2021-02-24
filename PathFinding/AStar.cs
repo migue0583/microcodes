426e920a7892ea6512f60303f44e0f9bbbdda040
migue0583 <migue0583@hotmail.com>
2021-02-24 17:37:11 -0600

    Added Node class inside the algorithm.

--- PathFinding/AStar.cs
+++ PathFinding/AStar.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.Collections.Generic;
 
 public class AStar
@@ -21,7 +20,7 @@ public class AStar
     ///     If the path is returned null, there is no way to reach the goal 
     ///     from the start.
     /// </returns>
-    public static List<Point> FindPath(Point start, Point goal, int[,] gridMatrix)
+    public static List<Node> FindPath(Node start, Node goal, int[,] gridMatrix)
     {
         //The quantity of rows and columns that the grid has
         int rowsQuantity = gridMatrix.GetLength(0);
@@ -31,17 +30,17 @@ public class AStar
         //Initially, only the start node is known.
         //This is usually implemented as a min-heap or priority queue 
         //rather than a hash-set or a list.
-        List<Point> openSet = new List<Point>();
+        List<Node> openSet = new List<Node>();
         openSet.Add(start);
 
         //For node n, cameFrom[n] is the node immediately 
         //preceding it on the cheapest path from start
         //to n currently known.
-        Dictionary<Point, Point> cameFrom = new Dictionary<Point,Point>();
+        Dictionary<Node, Node> cameFrom = new Dictionary<Node,Node>();
 
         //For node n, gScore[n] is the cost of the cheapest 
         //path from start to n currently known.
-        Dictionary<Point, double> gScore = new Dictionary<Point, double>();
+        Dictionary<Node, double> gScore = new Dictionary<Node, double>();
         //Map with default value of Infinity
         FillMapWithInfinity(gScore, rowsQuantity, colsQuantity);
         gScore[start] = 0;
@@ -49,7 +48,7 @@ public class AStar
         //For node n, fScore[n] := gScore[n] + h(n). 
         //fScore[n] represents our current best guess as to
         //how short a path from start to finish can be if it goes through n.
-        Dictionary<Point, double> fScore = new Dictionary<Point, double>();
+        Dictionary<Node, double> fScore = new Dictionary<Node, double>();
         //Map with default value of Infinity
         FillMapWithInfinity(fScore, rowsQuantity, colsQuantity);
         fScore[start] = h(start, goal);
@@ -59,7 +58,7 @@ public class AStar
             //This operation can occur in O(1) time if openSet is a 
             //min-heap or a priority queue
             //current := the node in openSet having the lowest fScore[] value
-            Point current = GetMinFromOpenSet(openSet, fScore);
+            Node current = GetMinFromOpenSet(openSet, fScore);
 
             if(current == goal)
             {//The goal is reached. Successful end of the algorithm.
@@ -67,10 +66,10 @@ public class AStar
             }
             openSet.Remove(current);
 
-            List<Point> neighborhood = GetNeighborhood(
-                current, gridMatrix);
+            List<Node> neighborhood = GetNeighborhood(
+                current, gridMatrix, rowsQuantity, colsQuantity);
 
-            foreach (Point neighbor in neighborhood)
+            foreach (Node neighbor in neighborhood)
             { 
                 // d(current,neighbor) is the weight of the edge from current to neighbor
                 // tentative_gScore is the distance from start to the neighbor through current
@@ -97,16 +96,16 @@ public class AStar
     //The d method stimates the distance 
     //to reach from node A to B, by calculating the euclidian
     //distance between the two nodes.
-    private static double d(Point nodeA, Point nodeB) {
-        return Math.Sqrt(Math.Pow(nodeB.X - nodeA.X, 2) 
-                            + Math.Pow(nodeB.Y - nodeA.Y, 2));
+    private static double d(Node nodeA, Node nodeB) {
+        return Math.Sqrt(Math.Pow(nodeB.Col - nodeA.Col, 2) 
+                            + Math.Pow(nodeB.Row - nodeA.Row, 2));
     }
 
     //This is the infamous Heuristic method/function.
     //In this case, it will stimate the effort 
     //required to reach from a node to the goal 
     //as the distances between them.
-    private static double h(Point node, Point goal)
+    private static double h(Node node, Node goal)
     {
         return d(node, goal);
     }
@@ -114,10 +113,10 @@ public class AStar
     //Creates a list tracing back the nodes 
     //traversed to reach the goal. This represents
     //the path found.
-    private static List<Point> ReconstructPath(
-        Dictionary<Point, Point> cameFrom, Point current)
+    private static List<Node> ReconstructPath(
+        Dictionary<Node, Node> cameFrom, Node current)
     {
-        List<Point> totalPath = new List<Point>();
+        List<Node> totalPath = new List<Node>();
         totalPath.Add(current);
 
         while (cameFrom.ContainsKey(current))
@@ -135,21 +134,21 @@ public class AStar
     //If the matrix has a value of 0, the node is not 
     //valid, and it is not added as a neighbor. 
     //Otherwise, it is added.
-    private static List<Point> GetNeighborhood(
-        Point node, int[,] adyMatrix)
+    private static List<Node> GetNeighborhood(
+        Node node, int[,] adyMatrix, int rowsQty, int colsQty)
     {
-        List<Point> neighborhood = new List<Point>();
+        List<Node> neighborhood = new List<Node>();
 
         for (int r = -1; r <= 1; r++)
         {
             for (int c = -1; c <= 1; c++)
             {
-                Point neighbor = new Point(node.X + c, node.Y + r);
+                Node neighbor = new Node(node.Col + c, node.Row + r);
                 if (neighbor == node)
                 {
                     continue;
                 }
-                if (IsValidNode(neighbor, adyMatrix))
+                if (IsValidNode(neighbor, adyMatrix, rowsQty, colsQty))
                 {
                     neighborhood.Add(neighbor);
                 }
@@ -159,37 +158,35 @@ public class AStar
     }
 
     private static bool IsValidNode(
-        Point node, int[,] gridMatrix)
+        Node node, int[,] gridMatrix, int rowsQty, int colsQty)
     {
-        int cantFilas = gridMatrix.GetLength(0);
-        int cantCols = gridMatrix.GetLength(1);
 
-        if (node.X < 0 || node.Y < 0)
+        if (node.Col < 0 || node.Row < 0)
         {//Outside the matrix. 
             return false;
         }
 
-        if (node.X >= cantCols || node.Y >= cantFilas)
+        if (node.Col >= colsQty || node.Row >= rowsQty)
         {//Outside the matrix. 
             return false;
         }
         //Returns true if the grid matrix has a value distinct than
         //zero in the reviewed node.
-        return gridMatrix[node.Y, node.X] != 0;
+        return gridMatrix[node.Row, node.Col] != 0;
     }
 
     //Traverses the openSet list looking for the
     //node with the lowest fScore value.
     //This method would be unnecessary if the 
     //openSet where a priority queue.
-    private static Point GetMinFromOpenSet(
-        List<Point> openSet, Dictionary<Point, double> fScore)
+    private static Node GetMinFromOpenSet(
+        List<Node> openSet, Dictionary<Node, double> fScore)
     {
         double minScore = int.MaxValue;
-        Point minNode = new Point(-1, -1);
+        Node minNode = null;
 
         //Traverse each nod in the open set.
-        foreach (Point node in openSet)
+        foreach (Node node in openSet)
         {
             double score = fScore[node];
             if (score <= minScore)
@@ -203,15 +200,56 @@ public class AStar
 
     //Prefills each node of the grid matrix with infinity as value.
     private static void FillMapWithInfinity(
-        Dictionary<Point, double> map, int rowsQty, int colsQty)
+        Dictionary<Node, double> map, int rowsQty, int colsQty)
     {
         for (int r = 0; r < rowsQty; r++)
         {
             for (int c = 0; c < colsQty; c++)
             {
-                Point node = new Point(c, r);
+                Node node = new Node(c, r);
                 map[node] = int.MaxValue;
             }
+        }
+    }
+
+    public class Node
+    {
+        public int Col;
+        public int Row;
+
+        public Node(int x, int y)
+        {
+            this.Col = x;
+            this.Row = y;
+        }
+
+        public static bool operator== (Node a, Node b)
+        {
+            return a.Col == b.Col && a.Row == b.Row;
+        }
+
+        public static bool operator !=(Node a, Node b)
+        {
+            return a.Col != b.Col || a.Row != b.Row;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Node)
+            {
+                return this == (Node)obj;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "[Row=" + Row + ", Col=" + Col + "]";
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
         }
     }
 
@@ -226,15 +264,15 @@ public class AStar
         };
          
         //Starting node at row 0, column 0
-        Point start = new Point(0, 0);
+        AStar.Node start = new AStar.Node(0, 0);
         //Goal node at row 4, column 2
-        Point goal = new Point(2, 4);
+        AStar.Node goal = new AStar.Node(2, 4);
 
-        List<Point> path = AStar.FindPath(start, goal, gridMatrix);
+        List<AStar.Node> path = AStar.FindPath(start, goal, gridMatrix);
         if (path != null)
         {
-            Console.WriteLine("This is the path from " + start + " to " + goal);  
-            foreach (Point p in path)
+            Console.WriteLine("This is the path from " + start + " to " + goal);
+            foreach (AStar.Node p in path)
             {
                 Console.WriteLine(p);
             }
@@ -247,4 +285,3 @@ public class AStar
         Console.Read();
     }
 }
-
